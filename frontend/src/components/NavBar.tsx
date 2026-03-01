76fe5397b5766ea8bacba5316ab05be0796ef73c
vascofcd <vasco.fcd@gmail.com>
2026-03-01 22:00:27 +0100

    styling

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,9 +2,11 @@ import { NavLink } from "react-router";
 
 export const NavBar = () => {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/create-market">Create Market</NavLink>
-    </nav>
+    <>
+      <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/create-market">Create</NavLink>
+      </div>
+    </>
   );
 };
